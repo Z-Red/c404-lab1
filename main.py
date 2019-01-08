4b7e402709899429e8ca9f56df18d5b2e66068a9
Z-Red <araien@ualberta.ca>
2019-01-08 10:08:26 -0700

    Resolve answer to question 8

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ import requests
 print(requests.__version__)
 print(requests.get("https://google.com").status_code)
 
+print("\n")
+print(requests.get("https://raw.githubusercontent.com/Z-Red/c404-lab1/master/main.py").text)
+
 
